307bb301e988572d9a0f9c209dcde4020224c053
kamishkorinskiy <lil.leet@bk.ru>
2022-06-24 18:52:25 +0300

    4.4

--- app.js
+++ app.js
@@ -37,5 +37,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+// view engine setup
+app.engine('ejs',require('ejs-locals'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 
 module.exports = app;
