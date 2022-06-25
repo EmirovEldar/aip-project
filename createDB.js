617bbb79aa08ee96b68a01813d3c1c152f20491a
kamishkorinskiy <lil.leet@bk.ru>
2022-06-25 13:32:39 +0300

    8.1

--- createDB.js
+++ createDB.js
@@ -1,12 +1,13 @@
-var MongoClient = require("mongodb").MongoClient
-var data = require("./data.js").data
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
 
+var Cat = mongoose.model('Cat', { name: String })
 
-MongoClient.connect("mongodb://localhost:27017/all", function(err,db){
-    if(err) throw err
-    db.dropDatabase()
-    var collection = db.collection("heroes")
-    collection.insertMany(data,function(err,result){
-        db.close()
-    })
+var kitty = new Cat({ name: 'Пушок' })
+kitty.save(function (err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Мяу')
+    }
 })
