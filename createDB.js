d60e4d659c94af8ee71784b88a68a8036c83de43
kamishkorinskiy <lil.leet@bk.ru>
2022-06-25 13:17:35 +0300

    6.2

--- createDB.js
+++ createDB.js
@@ -7,3 +7,6 @@ MongoClient.connect("mongodb://localhost:27017/all", function(err,db){
         db.close()
     })
 })
+var MongoClient = require("mongodb").MongoClient
+var data = require("./data.js").data
+
