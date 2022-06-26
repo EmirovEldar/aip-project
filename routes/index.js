b8652c8a95ae352537d84563d6e3f17431c91f42
kamishkorinskiy <lil.leet@bk.ru>
2022-06-26 18:18:35 +0300

    12.5

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var Hero = require("../models/hero").Hero
-var User = require("./../models/User").User
+var User = require("../models/User").User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,31 +13,40 @@ router.get('/', function(req, res, next) {
 
 });
 
+/* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
-    res.render('logreg',{title: 'Вход'});
+    res.render('logreg',{error:null});
 });   
+
+/* POST login/registration page. */
 router.post('/logreg', function(req, res, next) {
-  var username = req.body.username
-  var password = req.body.password
-  User.findOne({username:username},function(err,user){
-      if(err) return next(err)
-      if(user){
-          if(user.checkPassword(password)){
-              req.session.user = user._id
-              res.redirect('/')
-          } else {
-                  res.render('logreg', {title: 'Вход'})
-          }
-     } else {
-     var user = new User({username:username,password:password})
-          user.save(function(err,user){
-              if(err) return next(err)
-              req.session.user = user._id
-              res.redirect('/')
-          })        
-    }
-  })
+    var username = req.body.username
+    var password = req.body.password
+    User.findOne({username:username},function(err,user){
+        if(err) return next(err)
+        if(user){
+            if(user.checkPassword(password)){
+                req.session.user = user._id
+                res.redirect('/')
+            } else {
+                res.render('logreg',{error:"Пароль не верный"});
+            }
+        } else {
+        var user = new User({username:username,password:password})
+        user.save(function(err,user){
+            if(err) return next(err)
+            req.session.user = user._id
+            res.redirect('/')
+        })        
+        }
+    })
+});  
+
+/* GET auth page. */
+router.get('/logreg', function(req, res, next) {
+    res.render('logreg',{error:"Пароль не верный"});
 });
+
 
 
 
