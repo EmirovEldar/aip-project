846d63d709e80f59f2062f295dd646bd42f4d007
kamishkorinskiy <lil.leet@bk.ru>
2022-06-26 17:52:41 +0300

    12.2

--- data.js
+++ data.js
@@ -1,37 +1,37 @@
 var data = [{
     title: 'Блум',
     nick: 'blum',
-    avatar: 'images/blum.png',
+    avatar: '/images/blum.png',
     desc: 'Лучший друг Винни-Пуха, крошечный поросенок …'
 },
 {
     title: 'Стелла',
     nick: 'stella',
-    avatar: 'images/stella.png',
+    avatar: '/images/stella.png',
     desc: 'Старый серый ослик …'
 },
 {
     title: 'Муза',
     nick: 'muza',
-    avatar: 'images/muza.png',
+    avatar: '/images/muza.png',
     desc: 'Плюшевый медвежонок, игрушка … '
 },
 {
     title: 'Лейла',
     nick: 'leyla',
-    avatar: 'images/leyla.png',
+    avatar: '/images/leyla.png',
     desc: 'Сова жила в великолепном замке …'
 },
 {
     title: 'Техна',
     nick: 'tehna',
-    avatar: 'images/tehna.png',
+    avatar: '/images/tehna.png',
     desc: 'Кролик, всегда безумно рад видеть Винни-Пуха …'
 },
 {
     title: 'Флора',
     nick: 'flora',
-    avatar: 'images/flora.png',
+    avatar: '/images/flora.png',
     desc: 'Кролик, всегда безумно рад видеть Винни-Пуха …'
 }
 ];
